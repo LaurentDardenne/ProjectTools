b8f3694f9ec286f98753a82679ac5e7787fd519c
LaurentDardenne <LaDardenne@Orange.fr>
2016-09-16 13:56:07 +0200

    Tool for Pester

---
+++ Demos/Example New-TestSetParameter.ps1
@@ -0,0 +1,111 @@
+﻿#Démos de New-TestSetParameter
+
+. ..\New-TestParameterSet.ps1
+
+ #Get the metadata of a command ( unction/cmdlet)
+$cmd=Get-Command Test-Path
+
+#1- The command Test-path declare the follwinf parameters
+#   $cmd.Parameters.keys
+#   Path
+#   LiteralPath
+#   Filter
+#   Include
+#   Exclude
+#   PathType
+#   IsValid
+#   Credential
+#   OlderThan
+
+ #2 -New-TestSetParameter search if the caller declare one or more variable having name of those parameters
+ # No variable declared
+$result=New-TestSetParameter -command $Cmd  -ParameterSetNames Path
+ #No variable having a name of those parameters
+ #no result 
+
+$result.lines.count
+#0
+ 
+ #I want can include the 'Path' parameter in the result lines.
+ #I create a 'Path' variable to include it into the cartesian product  
+ #Each value inside the array create a 'Line'
+ 
+ #For   "'c:\temp\unknow.zip'", the function create the following line :
+ #  Test-Path -Path 'c:\temp\unknow.zip' 
+$Path=@(
+  "'c:\temp\unknow.zip'",
+  "'Test.zip'",
+  "(dir variable:OutputEncoding)",
+  "'A:\test.zip'",
+  "(Get-Item 'c:\temp')",
+  "(Get-Service Winmgmt)",
+  'Wsman:\*.*',
+  'hklm:\SYSTEM\CurrentControlSet\services\Winmgmt'
+)
+
+ #The parameter 'PathType' is a enum
+$PathType=@("'Container'", "'Leaf'")
+
+ #Generate the combinations of the parameter set of named 'Path'
+$result=New-TestSetParameter -command $Cmd  -ParameterSetNames Path
+ #The parameters that are not associated with a variable, generate a warning.
+ #   WARNING: The variable Filter is not defined, processing the next parameter.
+ #   WARNING: The variable Include is not defined, processing the next parameter.
+ #   WARNING: The variable Exclude is not defined, processing the next parameter.
+ #   WARNING: The variable IsValid is not defined, processing the next parameter.
+
+ #Number of built lines
+$result.lines.count
+#16
+
+$result
+# CommandName SetName Lines
+# ----------- ------- -----
+# Test-Path   Path    {Test-Path -Path 'c:\temp\unknow.zip' -PathType 'Container', Test-Path -Path 'Test.zip' -PathTyp...
+
+
+#Invoke the lines, *** Test-path have no impact ***  on the FileSystem
+$result.lines|% {Write-host $_ -fore green;$_}|Invoke-Expression
+
+#On ajoute le paramètre 'iSValide' de type booléen
+$isValid= @($true,$false)
+
+#Génére les combinaisons du jeu de paramètre nommée 'Path'
+$result=New-TestSetParameter -command $Cmd  -ParameterSetNames Path -WarningAction 'SilentlyContinue'
+$result.lines.count
+#32
+
+#génère :
+# Test-Path -Path 'c:\temp\unknow.zip' -PathType 'Container' -IsValid
+# Test-Path -Path 'A:\test.zip' -PathType 'Container' -IsValid
+# Test-Path -Path 'A:\test.zip' -PathType 'Leaf' -IsValid
+# Test-Path -Path 'A:\test.zip' -PathType 'Container'
+# Test-Path -Path 'A:\test.zip' -PathType 'Leaf'
+# ...
+
+#$Result.lines|Sort-Object|% {Write-host $_ -fore green;$_}|Invoke-Expression
+
+#On peut aussi générer du code de test pour Pester ou un autre module de test :
+$Template=@'
+#
+    It "Test ..TO DO.." {
+        try{
+          `$result = $_ -ea Stop
+        }catch{
+            Write-host "Error : `$(`$_.Exception.Message)" -ForegroundColor Yellow
+             `$result=`$false
+        }
+        `$result | should be (`$true)
+    }
+'@
+$Result.Lines| % { $ExecutionContext.InvokeCommand.ExpandString($Template) }
+#génère :
+#     It "Test ..TO DO.." {
+#         try{
+#           $result = Test-Path -Path 'c:\temp\unknow.zip' -PathType 'Container' -IsValid -ea Stop
+#         }catch{
+#             Write-host "Error : $($_.Exception.Message)" -ForegroundColor Yellow
+#             $result=$false
+#         }
+#         $result | should be ($true)
+#     }
---
+++ New-TestParameterSet.ps1
@@ -0,0 +1,139 @@
+﻿<#PSScriptInfo
+
+.VERSION 1.4
+.GUID a7b1444a-dc85-4841-84da-925f0309641c
+.AUTHOR Laurent Dardenne
+.COMPANYNAME
+.COPYRIGHT CopyLeft
+.TAGS Parameter ParameterSet Cartesian Pester Test
+.LICENSEURI https://creativecommons.org/licenses/by-nc-sa/4.0
+.PROJECTURI https://github.com/LaurentDardenne/ProjectTools
+.ICONURI
+.EXTERNALMODULEDEPENDENCIES
+.REQUIREDSCRIPTS
+.EXTERNALSCRIPTDEPENDENCIES
+.RELEASENOTES  version 1.0  le 25 mars 2013 
+
+.DESCRIPTION Generates the cartesian product of each parameter sets of a command  (tools for Pester)   
+#>
+
+function New-TestSetParameter{
+  [Diagnostics.CodeAnalysis.SuppressMessageAttribute("PSUseShouldProcessForStateChangingFunctions","",
+    Justification="New-TestSetParameter do not change the system state, only the application 'context'")]
+    
+#Generates the cartesian product of each parameter sets of a command  (tools for Pester)
+#adapted from : #http://makeyourownmistakes.wordpress.com/2012/04/17/simple-n-ary-product-generic-cartesian-product-in-powershell-20/
+ [CmdletBinding()]
+ [OutputType([system.string])]
+ param (
+   [parameter(Mandatory=$True,ValueFromPipeline=$True)]
+   [ValidateNotNull()]
+  [System.Management.Automation.CommandInfo] $CommandName,
+    
+    [ValidateNotNullOrEmpty()]
+    [Parameter(Position=0,Mandatory=$false)]
+  [string[]] $ParameterSetNames='__AllParameterSets',
+    
+    [ValidateNotNullOrEmpty()]
+  [string[]] $Exclude,
+  
+  [switch] $All
+ )       
+
+ begin {
+  function getValue{
+     if ($Value -eq $false)
+     {
+       Write-Debug "Switch is `$false, dont add the parameter name : $Result."
+       return "$result"
+     }
+     else
+     {
+       Write-Debug "Switch is `$true, add only the parameter name : $result$Bindparam"
+       return "$result$Bindparam"
+     }
+  }#getValue
+  
+  function AddToAll{
+   param (
+     [System.Management.Automation.CommandParameterInfo] $Parameter,
+     $valuesToAdd
+   )
+    Write-Debug "Treate '$($Parameter.Name)' parameter."
+    $Bindparam=" -$($Parameter.Name)" 
+      #Récupère une information du type du paramètre et pas la valeur liée au paramètre
+    $isSwitch=($Parameter.parameterType.FullName -eq 'System.Management.Automation.SwitchParameter')
+    Write-Debug "isSwitch=$isSwitch"
+    if ($null -ne $valuesToAdd)
+    {
+      foreach ($value in $valuesToAdd)
+      {
+        Write-Debug "Add Value : $value "
+        if ($Lines.Count -gt 0)
+        {
+          foreach ($result in $Lines)
+          {
+            if ($isSwitch) 
+            { Write-Output (getValue) } 
+            else
+            {
+              Write-Debug "Add parameter and value : $result$Bindparam $value"
+              Write-Output "$result$Bindparam $value"
+            }
+          }#foreach
+        }
+        else
+        {
+           if ($isSwitch) 
+           { Write-Output "$($CommandName.Name)$(getValue)" } 
+           else 
+           {
+             Write-Debug "Add parameter and value :: $Bindparam $value"
+             Write-Output  "$($CommandName.Name)$Bindparam $value"
+           }
+        }
+      }
+    }
+    else
+    {
+      Write-Debug "ValueToAdd is `$Null :$Lines"
+      Write-Output  $Lines
+    }
+  }#AddToAll  
+ }#begin
+
+  process {
+          
+   foreach ($Set in $CommandName.ParameterSets)
+   {
+      if (-not $All -and ($ParameterSetNames -notcontains $Set.Name)) 
+      { continue }
+      elseif ( $All -and ($Exclude -contains $Set.Name)) 
+      {
+        Write-Debug "Exclude $($Set.Name) "
+        continue
+      }
+      
+      $Lines = @()
+      Write-Debug "Current set name is $($Set.Name) "
+      Write-Debug "Parameter count=$($Set.Parameters.Count) "
+      foreach ($parameter in $Set.Parameters)
+      {
+        Write-Debug "Retrieve $($Parameter.Name) from caller"
+         #Module : Api V4 et >
+         #$Values= $MyInvocation.MyCommand.Module.GetVariableFromCallersModule($Parameter.Name) 
+        $Variable=Get-Variable -Name $Parameter.Name -Scope 1 -ea SilentlyContinue
+        if ( $null -ne $Variable) 
+        { $Lines = AddToAll -Parameter $Parameter $Variable.Value }
+        else
+        { $PSCmdlet.WriteWarning("The variable $($Parameter.Name) is not defined, processing the next parameter.") } 
+      }
+     New-Object PSObject -Property @{
+        PSTypeName='CartesianProductOfParameters'
+        CommandName=$CommandName.Name
+        SetName=$Set.Name
+        Lines=$Lines.Clone()
+     }
+   }#foreach
+  }#process
+} #New-TestSetParameter
