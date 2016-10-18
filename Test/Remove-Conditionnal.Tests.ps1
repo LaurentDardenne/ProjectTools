1059e80a4e8224b5a16b4fb6326aa85587c7e783
LaurentDardenne <LaDardenne@Orange.fr>
2016-10-18 17:19:59 +0200

    Fix include directive when the file do not exist

--- Test/Remove-Conditionnal.Tests.ps1
+++ Test/Remove-Conditionnal.Tests.ps1
@@ -1,40 +1,87 @@
 ﻿$here = Split-Path -Parent $MyInvocation.MyCommand.Path
 $sut = (Split-Path -Leaf $MyInvocation.MyCommand.Path) -replace '\.Tests\.', '.'
+. "$here\RCNewFiles.ps1"
+
+$here = $Here|Split-Path -Parent
 . "$here\$sut"
 
-. "$here\RCNewFiles.ps1"
-$PathSource='TestDrive:\RC'
+$PathSource="$Env:Temp\RC"
+. "$ProjectToolsVcs\Remove-Conditionnal.ps1"
+
+function normalizeEnds([string]$text)
+{
+    $text -replace "`r`n?|`n", "`r`n"
+}
+
 
 $TestCases=@(
- @{File"$PathSource\Test0.ps1";New="$PathSource\Test0.newC.ps1";Key='C'}
- @{File"$PathSource\Test0.ps1";New="$PathSource\Test0.newB.ps1";Key='B'}
- @{File"$PathSource\Test0.ps1";New="$PathSource\Test0.newA.ps1";Key='A'}
+ @{File="$PathSource\Test0.ps1";New="$PathSource\Test0.newC.ps1";Key='C'}
+ @{File="$PathSource\Test0.ps1";New="$PathSource\Test0.newB.ps1";Key='B'}
+ @{File="$PathSource\Test0.ps1";New="$PathSource\Test0.newA.ps1";Key='A'}
+)
+
+$TestCasesCombinations=@(
+ @{File="$PathSource\Combination1.ps1";New="$PathSource\Combination1-Debug.ps1";Key='Debug'}
+ @{File="$PathSource\Combination1.ps1";New="$PathSource\Combination1-Test.ps1";Key='Test'}
+ @{File="$PathSource\Combination1.ps1";New="$PathSource\Combination1-Debug+Test.ps1";Key=@('Debug','Test')}
 )
 
 $ErrorCases=@(
- @{File"$PathSource\Test05.ps1";Key='C'}
- @{File"$PathSource\Test06.ps1";Key='C'}
- @{File"$PathSource\Test07.ps1";Key='C'}
- @{File"$PathSource\Test09.ps1";Key='C'}
+ @{File="$PathSource\Test05.ps1";Key='C'}
+ @{File="$PathSource\Test06.ps1";Key='C'}
+ @{File="$PathSource\Test07.ps1";Key='C'}
+ @{File="$PathSource\Test09.ps1";Key='C'}
 )
 
-function Compare{
+function CompareStr{
  param([string[]]$Generate,[string[]]$Expected)         
  
  $set1 = New-Object System.Collections.Generic.HashSet[String](,$Generate)
  return ( $set1.SetEquals($Expected) ) 
 }
 
+
 Describe "Remove-Conditionnal" {
  
  Context "When there is no error" {
   
-  It "basic principle -Clean"{
-    [string[]]$A=Get-Content -Path "$PathSource\Test0.ps1"  -ReadCount 0 -Encoding UTF8 | Remove-Conditionnal -Clean 
-    [string[]]$B=Get-Content -Path "$PathSource\Test0.new.ps1" -ReadCount 0 -Encoding UTF8        
+  It "basic principle -Clean directives"{
+    [string[]]$A=Get-Content -Path "$PathSource\Test0-1.ps1"  -ReadCount 0 -Encoding UTF8 | Remove-Conditionnal -Clean 
+    [string[]]$B=Get-Content -Path "$PathSource\Test0-1.new.ps1" -ReadCount 0 -Encoding UTF8        
   
-    Compare $A $b | Should Be $true
+    CompareStr $A $B| Should Be $true
   } 
+
+
+  It "basic principle -Clean directives"{
+    [string[]]$A=Get-Content -Path "$PathSource\Combination1.ps1"  -ReadCount 0 -Encoding UTF8 | Remove-Conditionnal -Clean 
+    [string[]]$B=Get-Content -Path "$PathSource\Combination1-Clean.ps1" -ReadCount 0 -Encoding UTF8        
+  
+    CompareStr $A $B| Should Be $true
+  } 
+    
+  
+  It "basic principle -Remove directive"{
+    [string[]]$A=Get-Content -Path "$PathSource\Test0-2.ps1"  -ReadCount 0 -Encoding UTF8 | Remove-Conditionnal -Remove 
+    [string[]]$B=Get-Content -Path "$PathSource\Test0-2.new.ps1" -ReadCount 0 -Encoding UTF8        
+  
+    CompareStr $A $B| Should Be $true
+  } 
+
+ 
+  It "basic principle use 'Include' directive" {
+    [string[]]$A=Get-Content -Path "$PathSource\TestInclude1.ps1"  -ReadCount 0 -Encoding UTF8 | Remove-Conditionnal -Include 
+    [string[]]$B=Get-Content -Path "$PathSource\TestInclude1.new.ps1" -ReadCount 0 -Encoding UTF8        
+  
+    CompareStr $A $B| Should Be $true
+  }   
+
+  It "basic principle use 'Uncomment' directive" {
+    [string[]]$A=Get-Content -Path "$PathSource\TestUNCOMMENT.ps1"  -ReadCount 0 -Encoding UTF8 | Remove-Conditionnal -UnComment 
+    [string[]]$B=Get-Content -Path "$PathSource\TestUNCOMMENT.new.ps1" -ReadCount 0 -Encoding UTF8        
+  
+    CompareStr $A $B| Should Be $true
+  }   
 
   It "basic principle -ConditionnalsKeyWord" -TestCase $TestCases {
     Param(
@@ -42,21 +89,53 @@ Describe "Remove-Conditionnal" {
       [string]$New,
       [string]$Key
      )  
-
+    Write-Host "key=$key" 
     [string[]]$A=Get-Content -Path $File -ReadCount 0 -Encoding UTF8 |
                   Remove-Conditionnal -ConditionnalsKeyWord $Key |
                   Remove-Conditionnal -Clean 
      
     [string[]]$B=Get-Content -Path $New -ReadCount 0 -Encoding UTF8        
   
-    Compare $A $b | Should Be $true
+    CompareStr $A $B| Should Be $true
   } 
+  
+  It "basic principle -ConditionnalsKeyWord nested + combinations" -TestCase $TestCasesCombinations {
+    Param(
+      [string]$File,
+      [string]$New,
+      [string[]]$Key
+     )  
+    Write-Host "key=$key" 
+    [string[]]$A=Get-Content -Path $File -ReadCount 0 -Encoding UTF8 |
+                  Remove-Conditionnal -ConditionnalsKeyWord $Key
+
+    [string[]]$B=Get-Content -Path $New -ReadCount 0 -Encoding UTF8        
+  
+    CompareStr $A $B| Should Be $true                      
+  }
  }
 
  Context "When there error" {
   
+  It "basic principle use 'Include' directive, file do not exist" {
+    try {
+      $ErrorActionPreference = "Stop"
+      [string[]]$A=Get-Content -Path "$PathSource\TestInclude2.ps1"  -ReadCount 0 -Encoding UTF8 | 
+        Remove-Conditionnal -Include 
+    } 
+    catch
+    {
+      $_.FullyQualifiedErrorId | Should be 'PathNotFound,Microsoft.PowerShell.Commands.GetContentCommand'
+    } 
+  }   
+
+#todo file not exist : 'c:\temp\notexist.ps1' 
+#  "$PathSource\TestInclude3.ps1" -Force -Encoding UTF8
+ #todo ? It "basic principle use 'Uncomment' directive" {
+
   It "basic principle -ConditionnalsKeyWord. Nesting 'DEFINE' erroneous "{
    #peut importe la clé, la fonction valide l'ensemble des déclarations 
+    $ErrorActionPreference = "Stop"
     $Error.Clear()
     {Get-Content -Path "$PathSource\Test01.ps1"  -ReadCount 0 -Encoding UTF8 |
       Remove-Conditionnal -ConditionnalsKeyWord 'A' } | Should Throw
@@ -64,7 +143,7 @@ Describe "Remove-Conditionnal" {
   }
   
   It "basic principle -ConditionnalsKeyWord. Nesting 'UNDEF' erroneous "{
-   #peut importe la clé, la fonction valide l'ensemble des déclarations 
+    $ErrorActionPreference = "Stop"     
     $Error.Clear()
     {Get-Content -Path "$PathSource\Test02.ps1"  -ReadCount 0 -Encoding UTF8 |
       Remove-Conditionnal -ConditionnalsKeyWord 'A' } | Should Throw
@@ -72,7 +151,7 @@ Describe "Remove-Conditionnal" {
   }
 
   It "basic principle -ConditionnalsKeyWord. Nesting 'DEFINE' orphan"{
-   #peut importe la clé, la fonction valide l'ensemble des déclarations 
+    $ErrorActionPreference = "Stop"
     $Error.Clear()
     {Get-Content -Path "$PathSource\Test03.ps1"  -ReadCount 0 -Encoding UTF8 |
       Remove-Conditionnal -ConditionnalsKeyWord 'A' } | Should Throw
@@ -80,23 +159,23 @@ Describe "Remove-Conditionnal" {
   }  
 
   It "basic principle -ConditionnalsKeyWord. 'DEFINE' is not nested, but orphan"{
-   #peut importe la clé, la fonction valide l'ensemble des déclarations 
+    $ErrorActionPreference = "Stop"
     $Error.Clear()
-    {Get-Content -Path "$PathSource\Test031.ps1"  -ReadCount 0 -Encoding UTF8 |
-      Remove-Conditionnal -ConditionnalsKeyWord 'A' } | Should Throw
-    $Error.Count | Should be 1        
+    { Get-Content -Path "$PathSource\Test031.ps1"  -ReadCount 0 -Encoding UTF8 |
+       Remove-Conditionnal -ConditionnalsKeyWord 'A' } | Should Throw
+    $Error.Count | Should be 1         
   }  
 
   It "basic principle -ConditionnalsKeyWord. Nesting 'UNDEF' orphan"{
-   #peut importe la clé, la fonction valide l'ensemble des déclarations 
+    $ErrorActionPreference = "Stop"
     $Error.Clear()
-    {Get-Content -Path "$PathSource\Test04.ps1"  -ReadCount 0 -Encoding UTF8 |
+    {Get-Content -Path "$PathSource\Test04.ps1"  -ReadCount 0 -Encoding UTF8 | 
       Remove-Conditionnal -ConditionnalsKeyWord 'A' } | Should Throw
     $Error.Count | Should be 1        
   }  
 
   It "basic principle -ConditionnalsKeyWord. 'UNDEF' is not nested, but orphan"{
-   #peut importe la clé, la fonction valide l'ensemble des déclarations 
+    $ErrorActionPreference = "Stop"
     $Error.Clear()
     {Get-Content -Path "$PathSource\Test041.ps1"  -ReadCount 0 -Encoding UTF8 |
       Remove-Conditionnal -ConditionnalsKeyWord 'A' } | Should Throw
@@ -108,141 +187,20 @@ Describe "Remove-Conditionnal" {
       [string]$File,
       [string]$Key
      )      
-   #peut importe la clé, la fonction valide l'ensemble des déclarations 
+    Write-Host "`tFile : $File"
+    $ErrorActionPreference = "Stop" 
     $Error.Clear()
     {Get-Content -Path $File  -ReadCount 0 -Encoding UTF8 |
       Remove-Conditionnal -ConditionnalsKeyWord $Key } | Should Throw
     $Error.Count | Should be 1        
   } 
 
-#     
-#     It "does something useful" {
-# $Code=@'
-# #Test Imbrication de directive
-# Filter Test {
-# param (
-#     [String]$ConditionnalsKeyWord
-# )
-# Write-Host "Code de la fonction"
-# 
-# #<DEFINE %DEBUG%>
-# Write-Debug "$TempFile"
-# Write-Debug "$FullPath" 
-# #<UNDEF %DEBUG%>    
-# 
-# #<DEFINE %TEST%>     
-# Set-Location C:\Temp
-# #<DEFINE %DEBUG%>
-#   Write-Debug "Fin"
-# #<UNDEF %DEBUG%>     
-# "Remove-Conditionnal.ps1"|Remove-Conditionnal  "TEST"
-# #<UNDEF %TEST%>     
-# } #test
-# '@ > "$PathSource\Test4.ps1"     #bug avec Debug ET Test
-#         $true | Should Be $false
-#     }
-# 
-# 
-# 
-# 
-#     It "does something useful" {
-# $Code=@'
-# #Test erreur : Une directive sans mot clé de fin
-# Filter Test {
-# param (
-#     [String]$ConditionnalsKeyWord
-# )
-# Write-Host "Code de la fonction"
-# 
-# Write-Debug "$TempFile"
-# Write-Debug "$FullPath" 
-# #<UNDEF %DEBUG%>   
-# } #test
-# '@ > "$PathSource\TestErr0-1.ps1"   #bug pas détectée
-# 
-#         $true | Should Be $false
-#     }
-# 
-# 
-#     It "does something useful" {
-# 
-#         $true | Should Be $false
-#     }
-# $Code=@'
-# #Test erreur : deux directives. La première directive Debug est associè à la seconde directive Debug.
-# Filter Test {
-# param (
-#     [String]$ConditionnalsKeyWord
-# )
-# Write-Host "Code de la fonction"
-# 
-# #<DEFINE %DEBUG%>
-# Write-Debug "$TempFile"
-# Write-Debug "$FullPath" 
-# #<UN DEF %DEBUG%>    
-# 
-# #Imbrication de directive
-# #<DEFINE %TEST%>     
-# Set-Location C:\Temp
-# #<DE FINE %DEBUG%>
-#   Write-Debug "bug"
-# #<UNDEF %DEBUG%>     
-# "Remove-Conditionnal.ps1"|Remove-Conditionnal  "TEST"
-# #<UNDEF %TEST%>   
-#  
-# #<DEFINE %DEBUG%>
-#   Write-Debug "Fin"
-# #<UNDEF %DEBUG%>     
-#    
-# } #test
-# '@ > "$PathSource\TestErr3.ps1"  #Todo cas de bug 
-# # todo bug avec 'Debug' la directive %Test% n'est pas analysée car elle n'est pas dans la liste des directives
-# #on doit analyser le pattern est gérer $isDirectiveBloc seulement si la directive trouvée est dans celle demandée
-# #On teste le imbrication et le filtre séparément, ainsi on sait si la construction d'imbrication est correcte
-# 
-#         $true | Should Be $false
-#     }
-# 
-# 
-#     It "does something useful" {
-# $Code=@'
-# #Test erreur : deux directives. La première directive Debug est associè à la seconde directive Debug.
-# Filter Test {
-# param (
-#     [String]$ConditionnalsKeyWord
-# )
-# Write-Host "Code de la fonction"
-# 
-# #<DEFINE %DEBUG%>
-# Write-Debug "$TempFile"
-# Write-Debug "$FullPath" 
-# #<UNDEF %DEBUG%>    
-# 
-# #Imbrication de directive
-# #<DEFINE %TEST%>     
-# Set-Location C:\Temp
-# #<DEFINE %DEBUG%>
-#   Write-Debug "bug"
-# #<UNDEF %DEBUG%>     
-# "Remove-Conditionnal.ps1"|Remove-Conditionnal  "TEST"
-# #<UNDEF %TEST%>   
-#  
-# #<DEFINE %DEBUG%>
-#   Write-Debug "Fin"
-# #<UNDEF %DEBUG%>     
-#    
-# } #test
-# '@ > "$PathSource\TestErr4.ps1"  
-#         $true | Should Be $false
-#     }
-# 
-#     It "does something useful" {
-# 
-#         $true | Should Be $false
-#     }
-# 
-#     It "does something useful" {
-# 
-#         $true | Should Be $false
-#     }
+  It "basic principle -ConditionnalsKeyWord. 'DEFINE4 and 'UNDEF' are inverted" {
+    $ErrorActionPreference = "Stop" 
+    $Error.Clear()
+    {Get-Content -Path "$PathSource\Test09.ps1" -ReadCount 0 -Encoding UTF8 |
+      Remove-Conditionnal -ConditionnalsKeyWord 'NotExist' } | Should Throw
+    $Error.Count | Should be 1        
+  } 
+ }
 }
